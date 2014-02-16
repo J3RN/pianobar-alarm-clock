791875a364f0611ddbdfdbdfc65678bc7c415305
Jonathan Arnett <jonarnett90@gmail.com>
2014-02-16 18:14:26 -0500

    Made the alarm clock persistent

--- alarm-clock.rb
+++ alarm-clock.rb
@@ -7,17 +7,30 @@ in_up_time = gets
 # Parse user input time and current time
 up_time = Time.parse(in_up_time)
 
-# Nofity user
+# Notify user
 puts "I will wake you up at #{ up_time.strftime("%H:%M") }"
+
+alarm_turned_off = false
 
 # Continuously check for starting the music
 while true
   # Grab the current time, compare it to the wake up time
   curr_time = Time.now
   if curr_time.hour == up_time.hour and curr_time.min == up_time.min
-    puts "Wake up!"
-    %x(pianobar)
+    if not alarm_turned_off
+      puts "Wake up!"
+      pid = spawn("pianobar")
+      puts "Press any key to stop alarm..."
+      gets
+      Process.kill("SIGTERM", pid)
+      puts "\nGood morning!"
+      puts "I'll wake you up at #{ up_time.strftime("%H:%M") } tomorrow"
+      alarm_turned_off = true
+    end
   else
-    sleep 60.0
+    if alarm_turned_off
+      alarm_turned_off = false
+    end
+    sleep 30.0
   end
 end
